e1e6a99b7f7e5bf83486aec80895be51768fd6c1
dawiteu <dawid@tararuj.be>
2021-03-02 14:29:45 +0100

    CSS correction et cartes

---
+++ public/js/class/user.js
@@ -0,0 +1,8 @@
+export class User{ 
+    constructor(nom, prenom, classe, age){ 
+        this.nom = nom; 
+        this.prenom = prenom; 
+        this.classe = classe; 
+        this.age = age; 
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,29 @@
+import "./class/user.js"; 
+
+const abref = document.querySelector("a#abtnrefresh"); 
 // donnes user IP : 
 const ville = document.querySelector("span#ville"); 
 const pays = document.querySelector("span#pays");
 const oper = document.querySelector("span#oper"); 
+
+//register: 
+const btnReg = document.querySelector("button[name=registerbtn]");
+
+
+btnReg.addEventListener("click", () =>{
+    let regPrenom = document.querySelector("input[name=registerprenom]").value; 
+    let regNom = document.querySelector("input[name=registernom]").value; 
+    let regClasse= document.querySelector("input[name=registerclasse]").value; 
+    let regAge= document.querySelector("input[name=registerage]").value;
+
+
+    if(regPrenom.length > 0 && regNom.length > 0 && regClasse.length > 0 && regAge > 0){
+        let user = ""; 
+    }else{
+        alert('les champs doivent etre remplis!'); 
+    }
+});
+//fin register;'
 
 // donnees modal: 
 const btnModal = document.querySelector("a[href='#modalStag']"); 
@@ -13,41 +35,72 @@ let modalOpen = false;
 let modalClose = () =>{
     modal.style.visibility="hidden";
     modal.style.opacity="0"; 
-    document.body.classList.remove("hide");
-    modalOpen=false;   
-    // ici il faut changer la carte aussi (le active); 
+    let divH = document.querySelector("div.hide");
+    divH ? document.body.removeChild(divH) : null ; 
+    modalOpen=false;
 }
 
 btnModal.addEventListener("click", () => { 
     modalOpen=true; 
-    document.body.classList.add("hide");
-    modal.style.visibility="visible"; 
+    console.log(modal);
+    modal.style.visibility="visible";
     modal.style.opacity="1";
+    let divHide = document.createElement("div"); 
+    divHide.classList.add("hide"); 
+    document.body.insertBefore(divHide, document.querySelector("header"));
+    let bodyHei = document.body.offsetHeight; 
+    divHide.style.height = bodyHei; 
     modalC.addEventListener("click", () => {
         modalClose();
     });  
+
+    divHide.addEventListener("click", (e) => {
+        if(modalOpen){
+            if(e.target == e.currentTarget){
+                modalClose();   
+            }          
+        }
+    });
+
 }); 
 
+// fin modal ; 
 
-document.body.addEventListener("click", (e) => {
-    if(modalOpen){
-        if(e.target == e.currentTarget){
-            modalClose();   
-        }          
-    }
+// cartes (simulation); 
+const cartesLink = document.querySelectorAll("a.nav-link");
+const carttextes = document.querySelectorAll("div.textlink"); 
 
+
+cartesLink.forEach(card => {
+    card.addEventListener("click", () => {
+        let link = card.getAttribute('href').substring(1); 
+        if(link != "modalStag"){ // si c'est le modal on fait rien
+            cartesLink.forEach(e =>{ e.parentElement.classList.remove("active")});
+            card.parentElement.classList.add("active");   
+            divlink = card.getAttribute('href');
+            document.querySelector(divlink).style.display="block";     
+        }
+    });
 });
 
+//fin cartes
 
+
+
+// refresh: 
+abref.addEventListener("click", () =>{
+    let con = confirm("Voulez-vous refresh?");
+    con ? location.reload() : null; 
+});
 
 // csript user IP Api 
 const getIP = async() => {
     await fetch('http://ip-api.com/json/') // ${..ip..}; 
     .then(response => response.json())
     .then(response => {
-        ville.innerHTML = response.city; 
-        pays.innerHTML = response.country; 
-        oper.innerHTML = response.isp; 
+        //ville.innerHTML = response.city; 
+        //pays.innerHTML = response.country; 
+        //oper.innerHTML = response.isp; 
     })
 }
 
